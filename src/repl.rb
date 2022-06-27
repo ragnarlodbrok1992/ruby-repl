6b90c0fc324afdde3b185a730363d09cb7a7637f
Maciej Ragnar Oliwa <maraus92@gmail.com>
2022-06-27 12:01:54 +0200

    Added loop

--- src/repl.rb
+++ src/repl.rb
@@ -2,3 +2,15 @@ puts "Ruby REPL engine.";
 
 class REPL
 end
+
+def prompt(*args)
+    print(*args)
+    gets
+end
+
+while true
+    command = prompt "Ruby REPL -> "
+    if command.chomp == "exit"
+        break
+    end
+end
